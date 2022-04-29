ec6936949e08d881c5cf9a94d763925888fc5b9f
chenyuethu <chen.yue@kellogg.northwestern.edu>
2022-04-29 20:03:53 +0000

    1

--- code-along/2-models.rb
+++ code-along/2-models.rb
@@ -11,6 +11,10 @@ Company.destroy_all
 # - Insert, read, update, and delete rows in companies table
 #   (i.e. full CRUD of company data).
 
+# 1a. check out the schema file
+# 1b. check out the model file
+
+# 2. insert new rows in companies table
 
 new_company = Company.new
 new_company["name"] = "Apple"
@@ -34,17 +38,30 @@ new_company3.save
 
 puts "There are #{Company.all.count} companies."
 
-# 1a. check out the schema file
-# 1b. check out the model file
-
-# 2. insert new rows in companies table
 
 # 3. query companies table to find all row with California company
+# all_companies = Company.all
+# puts all_companies.inspect
+
+# cali_companies = Company.where({"state" => "CA"})
+# puts cali_companies.inspect
 
 # 4. query companies table to find single row for Apple
+# find_by: only one 
+apple = Company.find_by({"name" => "Apple"})
+# puts apple.inspect
 
 # 5. read a row's column value
+puts apple["url"]
 
 # 6. update a row's column value
+amazon = Company.find_by({"name" => "Amazon"})
+amazon["url"] = "http://amazon.com/"
+amazon.save
 
 # 7. delete a row
+twitter = Company.find_by({"name" => "Twitter"})
+twitter.destroy
+
+
+
