d14d9e93d1d8ae3ab52cbe7e374c277c2c4e2291
chenyuethu <chen.yue@kellogg.northwestern.edu>
2022-04-29 19:12:14 +0000

    3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_29_190137) do
+ActiveRecord::Schema.define(version: 2022_04_29_191043) do
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,14 @@ ActiveRecord::Schema.define(version: 2022_04_29_190137) do
     t.string "last_name"
     t.string "email"
     t.integer "company_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "salespeople", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
